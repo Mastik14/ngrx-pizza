275f434e5ca1d9251f39b2234a1262abba850fec
Mastik14 <www.sergeypygach@mail.ru>
2021-02-19 19:20:10 +0300

    first commit service approace

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './containers/app/app.component';
+
+// routes
+export const ROUTES: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  {
+    path: 'products',
+    loadChildren: () => import('../products/products.module').then(m => m.ProductsModule),
+  },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(ROUTES),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/products/services/toppings.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+import { Topping } from '../models/topping.model';
+
+@Injectable()
+export class ToppingsService {
+  constructor(private http: HttpClient) {}
+
+  getToppings(): Observable<Topping[]> {
+    return this.http
+      .get<Topping[]>(`/api/toppings`)
+      .pipe(catchError((error: any) => throwError(new Error(error.json()))));
+  }
+}
